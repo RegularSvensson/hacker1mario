e51490e21551882594363eb2307ac8c5de5ba86c
RegularSvensson <elias.svensson.1992@gmail.com>
2015-11-02 17:01:43 +0100

    print pyramid

--- mario.c
+++ mario.c
@@ -19,4 +19,32 @@ int main(void)
     
     // print height
     printf("You chose height: %i\n", height);
+    
+    // print pyramid
+    for (int row = 1; row <= height; row++)
+    {
+        // print left spaces
+        for (int spaces = 1; spaces <= height - row; spaces++)
+        {
+            printf(" ");
+        }
+        
+        // print left hashes
+        for (int hashes = 1; hashes <= row; hashes++)
+        {
+            printf("#");
+        }
+        
+        // print middle spaces
+        printf("  ");
+        
+        // print right hashes
+        for (int hashes = 1; hashes <= row; hashes++)
+        {
+            printf("#");
+        }
+        
+        //print new line
+        printf("\n");
+    }
 }
